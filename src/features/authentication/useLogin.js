49faaaba7c30e96de9e952bcafe8324f074c9e99
anac2828 <anac2828@yahoo.com>
2023-11-09 11:26:55 -0800

    fixed login bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       // This will put the user info on the Query cache so that the user is the fetched from supa base everytime the user info is needed
-      queryClient.setQueriesData(['user'], user);
+      queryClient.setQueryData(['user'], user.user);
       navigate('/dashboard', { replace: true });
     },
     onError: (err) => {
