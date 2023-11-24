5df5e7f3e3473688ae3e6f3233bb5a8875572356
Ana MacBook <anac2828@yahoo.com>
2023-11-24 12:27:32 -0800

    working on darkmode context provider

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { HiOutlineUser } from 'react-icons/hi2';
 import styled from 'styled-components';
 import ButtonIcon from './ButtonIcon';
+import DarkModeToggle from './DarkModeToggle';
 import Logout from '../features/authentication/Logout';
 
 const StyledHeaderMenu = styled.ul`
@@ -17,6 +18,9 @@ function HeaderMenu() {
         <ButtonIcon onClick={() => navigate('/account')}>
           <HiOutlineUser />
         </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeToggle />
       </li>
       <li>
         <Logout />
