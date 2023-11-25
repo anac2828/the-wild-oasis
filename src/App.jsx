903b535876209727e76b0b97f47ecc18ab0abda4
Ana MacBook <anac2828@yahoo.com>
2023-11-24 16:33:39 -0800

    done setting up dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import PageNotFound from './pages/PageNotFound';
 import AppLayout from './ui/AppLayout';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ProtectedRoute from './ui/ProtectedRoute';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 // staleTime is amount of the the data will be store in RQ cache.
 const queryClient = new QueryClient({
@@ -27,53 +28,55 @@ const queryClient = new QueryClient({
 // DECLARATIVE WAY OF SETTING UP ROUTES - We don't need to use useLoaders like in the pizza app so we are using the route setup
 function App() {
   return (
-    // Data provider with React-query
-    <QueryClientProvider client={queryClient}>
-      {/* Browser dev tools */}
-      <ReactQueryDevtools initialIsOpen={false} />
-      {/* CSS global styles */}
-      <GlobalSyles />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            element={
-              <ProtectedRoute>
-                <AppLayout />
-              </ProtectedRoute>
-            }>
-            {/* An index route is needed. AppLayout routes are children of the ProtectedRoute component also and will return route if use is authenticated*/}
-            <Route index element={<Navigate replace to='dashboard' />} />
-            <Route path='dashboard' element={<Dashboard />} />
-            <Route path='bookings' element={<Bookings />} />
-            <Route path='bookings/:bookingId' element={<Booking />} />
-            <Route path='cabins' element={<Cabins />} />
-            <Route path='checkin/:bookingId' element={<Checkin />} />
-            <Route path='users' element={<Users />} />
-            <Route path='settings' element={<Settings />} />
-            <Route path='account' element={<Account />} />
-          </Route>
+    <DarkModeProvider>
+      {/* // Data provider with React-query */}
+      <QueryClientProvider client={queryClient}>
+        {/* Browser dev tools */}
+        <ReactQueryDevtools initialIsOpen={false} />
+        {/* CSS global styles */}
+        <GlobalSyles />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }>
+              {/* An index route is needed. AppLayout routes are children of the ProtectedRoute component also and will return route if use is authenticated*/}
+              <Route index element={<Navigate replace to='dashboard' />} />
+              <Route path='dashboard' element={<Dashboard />} />
+              <Route path='bookings' element={<Bookings />} />
+              <Route path='bookings/:bookingId' element={<Booking />} />
+              <Route path='cabins' element={<Cabins />} />
+              <Route path='checkin/:bookingId' element={<Checkin />} />
+              <Route path='users' element={<Users />} />
+              <Route path='settings' element={<Settings />} />
+              <Route path='account' element={<Account />} />
+            </Route>
 
-          <Route path='login' element={<Login />} />
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster
-        position='top-center'
-        gutter={12}
-        containerStyle={{ margin: '8px' }}
-        toastOptions={{
-          success: { duration: 3000 },
-          error: { duration: 5000 },
-          style: {
-            fontSize: '16px',
-            maxWidth: '500px',
-            padding: '16px 24px',
-            backgroundColor: 'var(--color-grey-0)',
-            color: 'var(--color-grey-700)',
-          },
-        }}
-      />
-    </QueryClientProvider>
+            <Route path='login' element={<Login />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <Toaster
+          position='top-center'
+          gutter={12}
+          containerStyle={{ margin: '8px' }}
+          toastOptions={{
+            success: { duration: 3000 },
+            error: { duration: 5000 },
+            style: {
+              fontSize: '16px',
+              maxWidth: '500px',
+              padding: '16px 24px',
+              backgroundColor: 'var(--color-grey-0)',
+              color: 'var(--color-grey-700)',
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </DarkModeProvider>
   );
 }
 
--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,5 +1,7 @@
 import { createContext } from 'react';
 import { useLocalStorageState } from '../hooks/useLocalStorageState';
+import { useContext } from 'react';
+import { useEffect } from 'react';
 
 const DarkModeContext = createContext();
 
@@ -10,9 +12,28 @@ function DarkModeProvider({ children }) {
     setIsDarkMode((isDark) => !isDark);
   }
 
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark-mode');
+      document.documentElement.classList.remove('light-mode');
+    } else {
+      document.documentElement.classList.add('light-mode');
+      document.documentElement.classList.remove('dark-mode');
+    }
+  }, [isDarkMode]);
+
   return (
     <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
       {children}
     </DarkModeContext.Provider>
   );
 }
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined)
+    throw new Error('DarkModeContext was used outside DarkModeProvider.');
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };
