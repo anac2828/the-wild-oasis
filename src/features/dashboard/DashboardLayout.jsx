1c6fadf0891f7596f8b236bf209dbe46810a9081
anac2828 <anac2828@yahoo.com>
2023-11-28 20:57:16 -0800

    finished displaying sales and bookings on dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from 'styled-components';
+import { useRecentBookings } from '../dashboard/useRecentBookings';
+import { useRecentStays } from '../dashboard/useRecentStays';
+import { useCabins } from '../cabins/useCabins';
+import Stats from './Stats';
+import Spinner from '../../ui/Spinner';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,9 +13,19 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoadingStays, numDays } = useRecentStays();
+  const { cabins } = useCabins();
+  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
       <div>Today&lsquo;s activity</div>
       <div>Chart stay durations</div>
       <div>Chart sales</div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,16 +2,8 @@ import DashboardLayout from '../features/dashboard/DashboardLayout';
 import DashboardFilter from '../features/dashboard/DashboardFilter';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import Spinner from '../ui/Spinner';
-import { useRecentBookings } from '../features/dashboard/useRecentBookings';
-import { useRecentStays } from '../features/dashboard/useRecentStays';
 
 function Dashboard() {
-  const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
-  const { stays, confirmedStays, isLoading: isLoadingStays } = useRecentStays();
-
-  if ((isLoadingBookings, isLoadingStays)) return <Spinner />;
-
   return (
     <>
       <Row type='horizontal'>
