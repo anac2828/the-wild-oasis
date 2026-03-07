b791fb24c348d01ae0eeae3bafd1e7a8509a54e5
anac2828 <anac2828@yahoo.com>
2026-03-06 20:29:47 -0800

    fixed netlify.toml

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -6,6 +6,7 @@ const BASE_URL =
     ? import.meta.env.VITE_PROD_BASE_URL
     : import.meta.env.VITE_DEV_BASE_URL
 
+console.log(BASE_URL)
 // * SIGN UP
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
